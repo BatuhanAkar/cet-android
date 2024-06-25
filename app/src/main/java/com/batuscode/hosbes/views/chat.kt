43fcdd433aa8cdba64e17f14659ca1de9c9286a6
Batuhan <batuhanakr.akar@gmail.com>
2024-06-25 05:03:22 +0300

    hosbes

--- app/src/main/java/com/batuscode/hosbes/views/chat.kt
+++ app/src/main/java/com/batuscode/hosbes/views/chat.kt
@@ -258,23 +258,6 @@ fun Chat(mainActivityVM: MainActivityVM , chatViewModel: ChatViewModel){
                                     mainActivityVM.updateShowMore(true)
                                 }
                         )
-                    } else {
-                        Image(
-                            imageVector = Icons.Filled.AccountCircle ,
-                            contentDescription = "" ,
-                            contentScale = ContentScale.Fit,
-                            modifier = Modifier
-                                .clip(CircleShape)
-                                .width(30.dp)
-                                .height(30.dp)
-                                .clickable {
-                                    Log.d("chatProfilePicture", "clicked...")
-                                    mainActivityVM.updateShowMore(true)
-                                }
-                        )
-
-
-
                     }
 
 
@@ -399,6 +382,7 @@ fun Chat(mainActivityVM: MainActivityVM , chatViewModel: ChatViewModel){
 
 
 
+@RequiresApi(Build.VERSION_CODES.R)
 @Preview(showBackground = true , showSystemUi = true)
 @Composable
 fun ChatPreview(){
